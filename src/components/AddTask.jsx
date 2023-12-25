2d758dccdac7aa0b175448a4c5077d94f4b810bd
SudheerVegulla <sudheervegulla123@gmail.com>
2023-12-25 15:22:29 +0530

    removed commented changes

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -51,21 +51,9 @@ const AddTask = ({onSubmit}) => {
                 <div>
                 <label className="font-medium px-3" htmlFor="dueDate">Due date  :</label> 
                 <input className="border border-solid border-black rounded-lg p-1 m-2" id = "dueDate" type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-                {/* <DatePicker 
-                selected={date}
-                onChange={(date) => setDate(date)}
-                dateFormat="yyyy-mm-dd"
-                placeholderText="Select due date"
-                /> */}
                 </div>
-                {/* <button onClick={}>{}</button> */}
-                {/* <div>
-                    <label >Status :</label>
-                    <label><input type = "radio" value = "Incomplete" checked = {status === "Incomplete"} onChange = {handleRadioSelection} />Incomplete</label>
-                    <label><input type = "radio" value = "Complete" checked = {status === "Complete"} onChange = {handleRadioSelection} />Complete</label>
-                    </div> */}
-                    <button className = "bg-slate-400 rounded-lg p-2 m-2 flex m-auto"type = "submit"> Submit</button>
-                    {error && <p style={{color:"red"}}>{error}</p>}
+                <button className = "bg-slate-400 rounded-lg p-2 m-2 flex m-auto"type = "submit"> Submit</button>
+                {error && <p style={{color:"red"}}>{error}</p>}
             </form>
         </div>
     )
